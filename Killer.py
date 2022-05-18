94dd13e6c6e69b1a701802ba0ac65ed97d5bcd50
Mr. ALONE <101225561+KILLER-BOY-202@users.noreply.github.com>
2022-05-18 10:35:36 +0300

    Create Killer.py

---
+++ Killer.py
@@ -0,0 +1,8 @@
+import os, platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from killer9 import killer
+    killer()
+elif bit == '32bit':
+    exit('\x1b[1;91m\n\tOpps Your Device Not Supported')
