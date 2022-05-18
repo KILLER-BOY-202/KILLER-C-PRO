6790cb5214154fe88bd023496a5a410bd959be10
Mr. ALONE <101225561+KILLER-BOY-202@users.noreply.github.com>
2022-05-18 10:56:05 +0300

    Update Killer.py

--- Killer.py
+++ Killer.py
@@ -2,7 +2,7 @@ import os, platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from killer9 import killer
-    killer()
+    from killer9 import killerboy
+    killerboy()
 elif bit == '32bit':
     exit('\x1b[1;91m\n\tOpps Your Device Not Supported')
